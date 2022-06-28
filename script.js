2764d80e46dda48593a8b726e5925ba9b322e367
AMD <michojekunle1@gmail.com>
2022-06-28 06:49:47 -0700

    working on script file

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const submitButton = document.getElementById('addBook');
+const addBookBtn = document.getElementById('addBook');
+const author = document.getElementById('author');
+const isbn = document.getElementById('ISBN');
+const title = document.getElementById('title');
+const alertMessage = document.getElementById('alertText');
+const tableList = document.getElementById('books-listed');
+const removeBookBtn = document.getElementById('addBook');
 
 
