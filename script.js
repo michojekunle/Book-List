158112d003fcca784b6cd576dbfe0f93d65708b4
AMD <michojekunle1@gmail.com>
2022-06-28 08:12:15 -0700

    working with script file

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const addBookBtn = document.getElementById('addBook');
-const author = document.getElementById('author');
-const isbn = document.getElementById('ISBN');
-const title = document.getElementById('title');
-const alertMessage = document.getElementById('alertText');
-const tableList = document.getElementById('books-listed');
+const form = document.querySelector('#form-book');
 // const removeBookBtn = document.getElementById('');
+
+//book class
+class Book {
+    constructor(title, author, isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+} 
+
+//UI class
+class UI {
+    static 
+}
+
+
+//Local Storage
 
 
 
